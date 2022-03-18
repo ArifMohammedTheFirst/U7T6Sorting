1c23586abfb759e29efb7c6065aab9b081e90383
whh <wenhaoh5@nycstudents.net>
2022-03-18 14:01:49 -0400

    a

--- src/SortTester.java
+++ src/SortTester.java
@@ -5,12 +5,14 @@ public class SortTester
   public static void main(String[] args)
   {
     /* --- SELECTION SORT --- */
-    int[] arr1 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100, 101};
+    int[] arr1 = {49, 60, 89, 46, 88, 37, 75, 6, 96, 66, 75, 20, 76, 74, 71, 43,
+            62, 82, 24, 21};
     Sort.selectionSort(arr1);
     System.out.println(Arrays.toString(arr1));
  
     /* --- INSERTION SORT --- */ 
-    int[] arr2 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100, 101};
+    int[] arr2 = {49, 60, 89, 46, 88, 37, 75, 6, 96, 66, 75, 20, 76, 74, 71, 43,
+            62, 82, 24, 21};
     Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
   }
